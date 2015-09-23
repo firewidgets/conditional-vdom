b13d3d632466a5a41d9ec1a507438f58f7cf2f77
Christoph Dorn <christoph@christophdorn.com>
2015-09-22 21:46:28 -0700

    fixes

--- ch.js
+++ ch.js
@@ -12,20 +12,67 @@ module.exports = function (controllingState) {
 //console.log("conditionalControls", conditionalControls);
 
         function fillProperties (vtree) {
-            if (typeof conditionalControls.property !== "undefined") {
-                var val = null;
+
+            var getter = null;
+            if (typeof controllingState.get === "function") {
+                getter = function (name) {
+                    return controllingState.get(name);
+                };
+            } else {
+                getter = function (name) {
+                    return controllingState[name];
+                };
+            }
+
+            function setValueAndReturn (value) {
                 if (
-                    typeof controllingState.get === "function" &&
-                    typeof (val = controllingState.get(conditionalControls.property)) !== "undefined"
+                    vtree &&
+                    vtree.children &&
+                    vtree.children.length > 0
                 ) {
-//                    vtree.children[0].text = val;
-                    return val;
-                } else
-                if (typeof controllingState[conditionalControls.property] !== "undefined") {
-//                    vtree.children[0].text = controllingState[conditionalControls.property];
-                    return controllingState[conditionalControls.property];
+                    vtree.children[0].text = value;
+                    return vtree;
+                }
+                return value;
+            }
+
+            if (Array.isArray(vtree)) {
+                // TODO: Do this more elegantly during template parsing.
+                function forObject (obj) {
+                    for (var name in obj) {
+                        if (typeof obj[name] !== "string") continue;
+                        var re = /{{([^}]+)}}/g;
+        				var m = null;
+        				while ( (m = re.exec(obj[name])) ) {
+        				    obj[name] = obj[name].replace(m[0], getter(m[1]));
+        				}
+                    }
+                }
+                function forNodes (nodes) {
+                    nodes.forEach(function (node) {
+                        if (!node || !node.properties) return;
+                        if (node.properties.attributes) {
+                            forObject(node.properties.attributes);
+                        }
+                        if (node.properties.dataset) {
+                            forObject(node.properties.dataset);
+                        }
+                        // TODO: Only go as deep as next component (which will change 'getter' to access deeper data)
+                        if (node.children) {
+                            forNodes(node.children);
+                        }
+                    });
+                }
+                forNodes(vtree);
+            }
+
+            if (typeof conditionalControls.property !== "undefined") {
+                var val = getter(conditionalControls.property );
+                if (typeof val !== "undefined") {
+                    return setValueAndReturn(val);
                 }
             }
+
             return vtree;
         }
 
@@ -76,7 +123,7 @@ module.exports = function (controllingState) {
                     }
                 }
 
-                return conditionalSection();
+                return fillProperties(conditionalSection());
             });
             controllingState = controllingStateStack.pop();
             return result;
--- cvdom.js
+++ cvdom.js
@@ -6,7 +6,7 @@ exports.makeLib = function () {
         fs: require("fs"),
         _: require("lodash"),
         cvdom: exports,
-        html2hscript: require("../html2hscript"),
+        html2hscript: require("../html2chscript"),
         h: require("virtual-dom/h"),
         ch: require("./ch")
     }
@@ -17,7 +17,10 @@ exports.forLib = function (LIB) {
 
     function html2hscript (html, options, callback) {
 
+        html = html.replace(/^\s*|\s*$/g, "");
+
         return LIB.html2hscript(html, options, function (err, chscript, components, inlineScripts) {
+            if (err) return callback(err);
 
             return callback(null, chscript, components, inlineScripts);
         });

123131b675c4171106a3ca2d0300a87608e8f588
Christoph Dorn <christoph@christophdorn.com>
2015-11-03 23:02:48 -0800

    generate cjs code

--- cvdom.js
+++ cvdom.js
@@ -15,19 +15,54 @@ exports.makeLib = function () {
 
 exports.forLib = function (LIB) {
 
-    function html2hscript (html, options, callback) {
+    function html2chscript (html, options, callback) {
 
         html = html.replace(/^\s*|\s*$/g, "");
 
         return LIB.html2hscript(html, options, function (err, chscript, components, inlineScripts) {
             if (err) return callback(err);
 
-            return callback(null, chscript, components, inlineScripts);
+            var cjsCode = [];
+            cjsCode.push('module.exports = {');
+            cjsCode.push(  'getLayout: function () {');
+            cjsCode.push(    'return {');
+            cjsCode.push(      'buildVTree: function (h, ch) {');
+            cjsCode.push(        'return ' + chscript + ';');
+            cjsCode.push(      '}');
+            cjsCode.push(    '};');
+            cjsCode.push(  '},');
+            cjsCode.push(  'getComponents: function () {');
+            cjsCode.push(    'return {');
+            Object.keys(components).forEach(function (id, i) {
+                cjsCode.push(      (i>0?",":"") + '"' + id + '": {');
+                cjsCode.push(        'buildVTree: function (h, ch) {');
+                cjsCode.push(          'return ' + components[id].chscript + ';');
+                cjsCode.push(        '}');
+                cjsCode.push(      '}');
+            });
+            cjsCode.push(  '  };');
+            cjsCode.push(  '},');
+            cjsCode.push(  'getScripts: function () {');
+            cjsCode.push('var scripts = {};');
+            inlineScripts.forEach(function (inlineScript) {
+                cjsCode.push('if (!scripts["' + inlineScript.id + '"]) { scripts["' + inlineScript.id + '"] = {}; }');
+                cjsCode.push('scripts["' + inlineScript.id + '"]["' + inlineScript.location + '"] = function (exports) {');
+                    cjsCode.push(inlineScript.code);
+                cjsCode.push('};');
+            });
+            cjsCode.push('return scripts;');
+            cjsCode.push(  '}');
+            cjsCode.push('};');
+
+            return callback(null, chscript, components, inlineScripts, cjsCode.join("\n"));
         });
     }
 
     return {
-        html2hscript: html2hscript
+        html2chscript: html2chscript,
+
+        // DEPRECATED
+        html2hscript: html2chscript
     };
 }
 
